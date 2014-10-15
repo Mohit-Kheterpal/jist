b271000aa89e6073212a411e2129b25746a233d5
unknown-user <unknown-user@192.168.3.36>
2014-10-15 18:05:20 +0530

    Initial check-in of module sdfswerer

---
+++ EmptyClass23.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sdfswerer
+{
+	public class EmptyClass23
+	{
+		public EmptyClass23 ()
+		{
+		}
+	}
+}
+
---
+++ EmptyClass32.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sdfswerer
+{
+	public class EmptyClass32
+	{
+		public EmptyClass32 ()
+		{
+		}
+	}
+}
+
